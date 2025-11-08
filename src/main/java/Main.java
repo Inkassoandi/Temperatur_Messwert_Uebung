d70ab7785dca4c1d84d74f4c9a696f58f5b6519d
Alexander Regenfelder (3AAIF) <alexander.regenfelder@edu.htl-klu.at>
2025-11-08 15:09:04 +0100

    in der Klasse SortBy, eine Methode implementiert, welche aus einer Liste alle Messungen eines gewünschten Bundeslands in eine neue Liste speichert und zurückgibt.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
 
-        /**
+        /*
          * Hier wird die FileHandler Methode angewendet um mit ihr eine Texdatei von Messungen
          * einzuselen und in in einer Liste von "Bundesländern abzuspeichern.
          */
@@ -15,7 +15,14 @@ public class Main {
         System.out.println();
         System.out.println(tempMessungen.getFirst());
         System.out.println();
-        System.out.println(tempMessungen.get(0));
+
+
+        // Aus einer Liste alle Kärntner Messungen in eine neue Liste Speichern
+        List <Kärnten> tempKärnten = SortBy.getKärnten(tempMessungen);
+
+        tempKärnten.forEach(System.out::println);
+        System.out.println();
+
 
     }
 }
--- src/main/java/SortBy.java
+++ src/main/java/SortBy.java
@@ -1,17 +1,96 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class SortBy {
 
     public static List<Kärnten> getKärnten(List<Bundesland> incomeList){
         List<Kärnten> outcomeList = new ArrayList<>();
         for(Bundesland object: incomeList){
-            //if (object.equals());
+            if (Objects.equals(object.getName(), "Kärnten")){
+                outcomeList.add((Kärnten) object);
+            }
         }
-
-        return null;
+        return outcomeList;
     }
 
+    public static List<Steiermark> getSteiermark(List<Bundesland> incomeList){
+        List<Steiermark> outcomeList = new ArrayList<>();
+        for(Bundesland object: incomeList){
+            if (Objects.equals(object.getName(), "Steiermark")){
+                outcomeList.add((Steiermark) object);
+            }
+        }
+        return outcomeList;
+    }
 
+    public static List<Salzburg> getSalzburg(List<Bundesland> incomeList){
+        List<Salzburg> outcomeList = new ArrayList<>();
+        for(Bundesland object: incomeList){
+            if (Objects.equals(object.getName(), "Salzburg")){
+                outcomeList.add((Salzburg) object);
+            }
+        }
+        return outcomeList;
+    }
 
+    public static List<Burgenland> getBurgenland(List<Bundesland> incomeList){
+        List<Burgenland> outcomeList = new ArrayList<>();
+        for(Bundesland object: incomeList){
+            if (Objects.equals(object.getName(), "Burgenland")){
+                outcomeList.add((Burgenland) object);
+            }
+        }
+        return outcomeList;
+    }
+
+    public static List<Wien> getWien(List<Bundesland> incomeList){
+        List<Wien> outcomeList = new ArrayList<>();
+        for(Bundesland object: incomeList){
+            if (Objects.equals(object.getName(), "Wien")){
+                outcomeList.add((Wien) object);
+            }
+        }
+        return outcomeList;
+    }
+
+    public static List<Niederösterreich> getNiederösterreich(List<Bundesland> incomeList){
+        List<Niederösterreich> outcomeList = new ArrayList<>();
+        for(Bundesland object: incomeList){
+            if (Objects.equals(object.getName(), "Niederösterreich")){
+                outcomeList.add((Niederösterreich) object);
+            }
+        }
+        return outcomeList;
+    }
+
+    public static List<Oberösterreich> getOberösterreich(List<Bundesland> incomeList){
+        List<Oberösterreich> outcomeList = new ArrayList<>();
+        for(Bundesland object: incomeList){
+            if (Objects.equals(object.getName(), "Oberösterreich")){
+                outcomeList.add((Oberösterreich) object);
+            }
+        }
+        return outcomeList;
+    }
+
+    public static List<Tirol> getTirol(List<Bundesland> incomeList){
+        List<Tirol> outcomeList = new ArrayList<>();
+        for(Bundesland object: incomeList){
+            if (Objects.equals(object.getName(), "Tirol")){
+                outcomeList.add((Tirol) object);
+            }
+        }
+        return outcomeList;
+    }
+
+    public static List<Vorarlberg> getVorarlberg(List<Bundesland> incomeList) {
+        List<Vorarlberg> outcomeList = new ArrayList<>();
+        for (Bundesland object : incomeList) {
+            if (Objects.equals(object.getName(), "Vorarlberg")) {
+                outcomeList.add((Vorarlberg) object);
+            }
+        }
+        return outcomeList;
+    }
 }
